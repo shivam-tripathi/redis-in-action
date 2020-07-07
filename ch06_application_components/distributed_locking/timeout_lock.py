20dbee07d09ce3a6a3d613ffd3eb4abd8c57a28a
Shivam Tripathi <shivamtripathi0108@gmail.com>
2020-07-08 01:05:44 +0530

    feat(ch06_application_components): distributed_locking - timeout_lock

---
+++ ch06_application_components/distributed_locking/timeout_lock.py
@@ -0,0 +1,29 @@
+import redis
+import uuid
+
+class TimeoutLock:
+	def __init__(self):
+		self.conn = redis.Redis()
+
+
+	@staticmethod
+	def get_lock_key(lockname):
+		return f'lock:{lockname}'
+
+
+	def acquire_lock_with_timeout(self, lockname, acquire_timeout, lock_timeout):
+		identifier = uuid.uuid4()
+		lockkey = get_lock_key(lockname)
+		lock_timeout_int = int(math.ceil(lock_timeout))
+		acquire_timeout_timestamp = time.time() + acquire_timeout
+
+		while time.time() < acquire_timeout_timestamp:
+			if conn.setnx(lockkey, identifier):
+				conn.expire(lockkey, lock_timeout_int)
+				return identifier
+			elif not conn.ttl(lockkey):
+				conn.expire(lockkey, lock_timeout_int)
+
+			sleep(0.001)
+
+		return False
